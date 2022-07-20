0495e9cd0e1f0399513d0fa44f3854a1e8db72da
Dweina1920 <dina.yamanid@gmail.com>
2022-07-20 22:31:43 +0200

    home

--- src/Views/components/header.php
+++ src/Views/components/header.php
@@ -7,15 +7,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Releer</title>
-    <link href="C:\xampp\htdocs\releerapp\src\css\style.css" rel="stylesheet" type="text/css">
+    
+    <link rel="stylesheet" type="text/css" href="../../assets/css/style.css">
+
   
     
 </head>
 <body>
     <header class="header">
         <div>
-            <img src= "" alt="logo"></img>
+            <img class="img-logo-header" src="../../img/logo-navbar.png" alt="logo"></img>
+        </div>
+        <div class="div-header">
+            <input class= "input-navbar" type="text" value="  Buscar..."></input>
+            <button class= "button-publicar"> + Publique su anuncio</button>
+            <button class= "button-filtros">   Filtros</button>
         </div>
     </header>
+
+
 </body>
 </html>
--- src/Views/home.php
+++ src/Views/home.php
@@ -1,4 +1,6 @@
+
 <?php  ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,10 +8,172 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="CSS/menu.css">
-  <link rel="stylesheet" type="text/css" href="css/style.css" />
+    
+  <link rel="stylesheet" type="text/css" href="../assets/css/style.css" />
+  
+  
 </head>
 <body>
-<h1 class="huno">hola</h1>
+
+<header class="header">
+  <div>
+    <img class="img-logo-header" src="../img/logo-navbar.png" alt="logo"></img>
+  </div>
+  <div class="div-header">
+              <input class= "input-navbar" type="text" value="  Buscar..."></input>
+              <button class= "button-publicar"> + Publique su anuncio</button>
+              <button class= "button-filtros">   Filtros</button>
+
+  </div>
+</header>
+    <main>
+        <div class="div-img-informacion">
+            <img class="img-informacion" src="../img/importancia-de-leer.png" alt="banner"/>
+        </div>
+
+        <div class="subidos-recientemente">
+          <p class="texto-subidos-recientemente">Subidos recientemente</p>
+        </div>
+
+        <div class="container-books">
+          <div class="book-box">
+            <div class="div-img-box">
+              <img class="img-box" src="../img/portada.jpg" alt="imagen libro"/>
+            </div>
+            <div class=info-box>
+              <p class="titulo-box">Maestra</p>
+              <p class="autor-box">LS Hilton</p>
+              <p class="titulo-box">10€</p>
+            </div>
+          </div>
+        </div>
+    </main>
+
+
+
+
+    <div class="container-books">
+          <div class="book-box">
+            <div class="div-img-box">
+              <img class="img-box" src="../img/portada.jpg" alt="imagen libro"/>
+            </div>
+            <div class=info-box>
+              <p class="titulo-box">Maestra</p>
+              <p class="autor-box">LS Hilton</p>
+              <p class="titulo-box">10€</p>
+            </div>
+          </div>
+
+
+
+
+          <div class="book-box">
+            <div class="div-img-box">
+              <img class="img-box" src="../img/portada.jpg" alt="imagen libro"/>
+            </div>
+            <div class=info-box>
+              <p class="titulo-box">Maestra</p>
+              <p class="autor-box">LS Hilton</p>
+              <p class="titulo-box">10€</p>
+            </div>
+          </div>
+
+
+
+
+          <div class="book-box">
+            <div class="div-img-box">
+              <img class="img-box" src="../img/portada.jpg" alt="imagen libro"/>
+            </div>
+            <div class=info-box>
+              <p class="titulo-box">Maestra</p>
+              <p class="autor-box">LS Hilton</p>
+              <p class="titulo-box">10€</p>
+            </div>
+          </div>
+
+
+
+
+          <div class="book-box">
+            <div class="div-img-box">
+              <img class="img-box" src="../img/portada.jpg" alt="imagen libro"/>
+            </div>
+            <div class=info-box>
+              <p class="titulo-box">Maestra</p>
+              <p class="autor-box">LS Hilton</p>
+              <p class="titulo-box">10€</p>
+            </div>
+          </div>
+
+
+
+
+          <div class="book-box">
+            <div class="div-img-box">
+              <img class="img-box" src="../img/portada.jpg" alt="imagen libro"/>
+            </div>
+            <div class=info-box>
+              <p class="titulo-box">Maestra</p>
+              <p class="autor-box">LS Hilton</p>
+              <p class="titulo-box">10€</p>
+            </div>
+          </div>
+
+
+
+
+          <div class="book-box">
+            <div class="div-img-box">
+              <img class="img-box" src="../img/portada.jpg" alt="imagen libro"/>
+            </div>
+            <div class=info-box>
+              <p class="titulo-box">Maestra</p>
+              <p class="autor-box">LS Hilton</p>
+              <p class="titulo-box">10€</p>
+            </div>
+          </div>
+
+
+          <div class="book-box">
+            <div class="div-img-box">
+              <img class="img-box" src="../img/portada.jpg" alt="imagen libro"/>
+            </div>
+            <div class=info-box>
+              <p class="titulo-box">Maestra</p>
+              <p class="autor-box">LS Hilton</p>
+              <p class="titulo-box">10€</p>
+            </div>
+          </div>
+
+
+
+
+
+
+          <div class="book-box">
+            <div class="div-img-box">
+              <img class="img-box" src="../img/portada.jpg" alt="imagen libro"/>
+            </div>
+            <div class=info-box>
+              <p class="titulo-box">Maestra</p>
+              <p class="autor-box">LS Hilton</p>
+              <p class="titulo-box">10€</p>
+            </div>
+          </div>
+
+
+          
+        </div>
+    </main>
+
+
+
+
+    
+
+
+    
+
 </body>
 </html>
