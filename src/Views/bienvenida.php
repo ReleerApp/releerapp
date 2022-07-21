d3fb04e850f75cfe2f7024b3a028fd5fe298e5eb
pruizcas <pruizcas@gmail.com>
2022-07-21 14:56:22 +0200

    first commit

---
+++ src/Views/bienvenida.php
@@ -0,0 +1,25 @@
+<?php  
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+    
+  <link rel="stylesheet" type="text/css" href= "../assets/css/style.css"/>
+
+</head>
+
+<body>
+    <main class="prueba">
+    <img class= "logo" src="src/img/logoreleer.png">
+    <h3>Reutiliza tu conocimiento</h3>
+    <button type="button" class="buttonwelcome" href=".../src/Views/home.php">Quiero comprar</button>
+    <button type="button" class="buttonwelcome" href=".../src/Views/vender.php">Quiero vender</button>
+    </main>
+</body>
+
+</html>
